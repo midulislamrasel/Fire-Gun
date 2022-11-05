2bd56bb3ff3da9d81bf9356c9b0f7be98dc307e7
Midul-islam-Russell <midulraddy121@gmail.com>
2022-11-06 01:18:45 +0600

    card

--- src/component/AllGuns/AllGuns.js
+++ src/component/AllGuns/AllGuns.js
@@ -14,6 +14,8 @@ const AllGuns = () => {
     
     return (
         <div className='all_guns'>
+            <h1 className='text-center mt-4 text-4xl'>Welcome to Fire Guns</h1>
+            <p className='text-center mt-4 text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolores libero ad assumenda dignissimos quam rerum voluptatibus ducimus repellat eaque pariatur maiores, maxime mollitia iure molestias consequatur recusandae. Earum, consectetur.</p>
             {
                 guns?.map(gun => <SingelGun gun={gun} key={gun.id}></SingelGun>)
             }
--- src/component/SingelGun/SingelGun.js
+++ src/component/SingelGun/SingelGun.js
@@ -6,7 +6,24 @@ const SingelGun = (props) => {
     
     return (
         <div>
-            <h1 className='text-center text-4xl'>Text</h1>
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                    {name}
+                    <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>Bullet : {bullet}</p>
+                    <p>Capacity : {capacity}</p>
+                    <p>Action : {action}</p>
+                    <p>Category : {category}</p>
+                    <p>Price : {price}</p>
+                    <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Fashion</div> 
+                    <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
